d3b9ffeffe5f0b57ea107b5b3f01fdc7be6602bb
Emilio <emilioaperezvilla@outlook.com>
2023-03-08 18:51:25 -0600

    distancia al objetivo

--- Buscador_de_Rutas.py
+++ Buscador_de_Rutas.py
@@ -7,7 +7,8 @@ np.set_printoptions(threshold=sys.maxsize)
 img = cv.imread('hilo1.png',cv.IMREAD_GRAYSCALE)
 matrix=np.matrix(img)
 matrix2=np.where( matrix == 255 ,1,0)
-objetivo=[256,256]
+objetivo=[255,255]
+estado_inicial=[0,0]
 
 def test_objetivo(estado): 
     if estado==objetivo:
@@ -23,12 +24,14 @@ def costo_entre_dos(estado1, estado2):
     return 1
 
 def distancia_al_objetivo(estado):
-    i= ciudades.index(estado)
-    j=ciudades.index(objetivo)
-    x1= coordenadas[i][0]
-    x2=coordenadas[j][0]
-    y1= coordenadas[i][1]
-    y2= coordenadas[j][1]
+    x1=estado[0]
+    y1=estado[1]
+    x2=objetivo[0]
+    y2=objetivo[1]
+    # x1=coordenadas[i][0]
+    # x2=objetivo[j][0]
+    # y1= coordenadas[i][1]
+    # y2= objetivo[j][1]
     return math.sqrt(pow(x1-x2,2)+pow(y1-y2,2))
 
 def en_pantalla(lista_estados):
@@ -59,14 +62,24 @@ def dentro_de(estado, lista_de_estados):
             return 1
     return 0
 
+def hacer_nodo(estado, i_padre):
+    d=distancia_al_objetivo(estado)
+    if i_padre==-1: 
+        n=[estado,0, -1, 1,0,0,d ] 
+    else:
+        c=nodos[i_padre][4]+costo_entre_dos(estado,nodos[i_padre][0])
+        n=[estado,len(nodos), i_padre, nodos[i_padre][3]+1, c, c+d]
+    nodos.append(n)
+    return n
+
 print("Escribe la estrategia que quieres seguir para la búsqueda. Elige escribiendo el número que corresponde")
 print("1: Búsqueda en Profundidad")
 print("2: Búsqueda en Amplitud")
 print("3: Búsqueda primero el mejor, por costos")
 print("4: Búsqueda A*")
 E=input("Tu elección: ")
-# print("En esta ocasión se busca el camino:")
-# en_pantalla([estado_inicial, objetivo])
+print("En esta ocasión se busca el camino:")
+en_pantalla([estado_inicial, objetivo])
 
 visto=[]
 nodos =[]
@@ -75,12 +88,17 @@ solucion=[]
 frontera.append(hacer_nodo(estado_inicial, -1))
 
 while len(frontera)!=0:
+    #saca un elemento de la frontera para explorar sus sucesores, esto depende de la estrategia
     if E=="1": 
-        nodo= frontera.pop()
+        nodo= frontera.pop()  # saca al útimo
     else: 
-        nodo= frontera.pop(0) 
+        nodo= frontera.pop(0)  # 0 saca el primero
+    
+    # guardo el estado que saque de la fronteda en los "vistos" para no volver a explorarlo
     visto.append(nodo[0])
-if test_objetivo(nodo[0])==1:
+   
+   
+    if test_objetivo(nodo[0])==1:
         print("*****solución encontrada*****")
         print("Pasos para llegar a la solución: {}".format(nodo[3]-1))
         print("Costo total de la solución: {}".format(nodo[4]))
@@ -97,6 +115,7 @@ if test_objetivo(nodo[0])==1:
             if  dentro_de(x,visto)==0: 
                 nuevo_nodo= hacer_nodo(x,nodo[1])
                 if E=="4":
+                    # esta es una insención ordenada de acuerdo al elemento 5 del nodo, que es la heurística
                     cuenta=len(frontera)
                     for i in range(cuenta):
                         if nuevo_nodo[5]<frontera[i][5]:
@@ -104,7 +123,9 @@ if test_objetivo(nodo[0])==1:
                             break
                     if cuenta== len(frontera):
                         frontera.append(nuevo_nodo)
+                    #fin de la inseción ordenada
                 if E=="3":
+                    # esta es una insención ordenada de acuerdo al elemento 4 del nodo, que es el costo del camino
                     cuenta=len(frontera)
                     for i in range(cuenta):
                         if nuevo_nodo[4]<frontera[i][4]:
@@ -112,6 +133,8 @@ if test_objetivo(nodo[0])==1:
                             break
                     if cuenta== len(frontera):
                         frontera.append(nuevo_nodo)
+                        #fin de la inserción ordenada
                 if E=="1" or E=="2":
+                    # en el caso de las búsquedas desinformadas, simplemente se inserta cada sucedor al final de la frontera
                     frontera.append(nuevo_nodo) 
 print("búsqueda finalizada con {} nodos explorados".format(len(nodos)))
