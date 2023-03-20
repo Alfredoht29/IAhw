1595c2b8646a8d479b4437bafaf8a6f494d5b63c
Alfredoht29 <emilioaperezvilla@outlook.com>
2023-03-20 11:24:47 -0600

    resuelto

--- Buscador_de_Rutas.py
+++ Buscador_de_Rutas.py
@@ -5,33 +5,35 @@ import math
 
 np.set_printoptions(threshold=sys.maxsize)
 img = cv.imread('hilo1.png',cv.IMREAD_GRAYSCALE)
-matrix=np.array(img)
-matrix2=np.where( matrix == 255 ,1,0)
-objetivo=[255,255]
-vecinos=[]
+img2= cv.imread('hilo1.png')
+matrix2=np.where( img == 255 ,1,0)
+objetivo=[253,253]
 estado_inicial=[0,0]
-cicp=[]
 
-def encuentra_camino():
-    fila,columna=np.where(matrix2==0)
-    return list(zip(fila,columna))
-    
-cicp=encuentra_camino()
-vecinos = [[i, j] for i, j in cicp]
-ciudades = [[i, j] for i in range(len(matrix2)) for j in range(len(matrix2[i]))]
-
-def test_objetivo(estado): 
-    if ((estado[0]==objetivo[0])and(estado[1]==objetivo[1])):
+def test_objetivo(estado):
+    if ((estado[0]>=objetivo[0])and(estado[1]>=objetivo[1])):
         return 1
     else:
         return 0
         
-def funcion_sucesor(estado): 
-    i= ciudades.index(estado)
-    return vecinos[i]
+def funcion_sucesor(estado):
+    nl=[]
+    r=tuple([estado[0]+1,estado[1]])
+    l=tuple([estado[0]-1,estado[1]])
+    u=tuple([estado[0],estado[1]-1])
+    d=tuple([estado[0],estado[1]+1])
+    if(matrix2[r]==0 and r[0]<255 and r[1]<255):
+        nl.append(r)
+    if(matrix2[l]==0 and l[0]<255 and l[1]<255):
+        nl.append(l)
+    if(matrix2[u]==0 and u[0]<255 and u[1]<255):
+        nl.append(u)
+    if(matrix2[d]==0 and d[0]<255 and d[1]<255):
+        nl.append(d)
+    return nl
     
-def costo_entre_dos(estado1, estado2):   
-    return 2
+def costo_entre_dos(estado1, estado2):
+    return 1
     
 def distancia_al_objetivo(estado):
     x1= estado[0]
@@ -47,15 +49,19 @@ def en_pantalla(lista_estados):
         else:
             print("{} ->".format(x), end = ' ')
 
+def pintar_camino(lista_estados):
+	for x in lista_estados:
+		img2[x]=[255,255,0]	
+	cv.imshow("img",img2)	
+	cv.waitKey(0)
 # ##### Despues de esto no se edita nada de abajo
 # ********************************************************
 # ********************************************************
 # ********************************************************
-
 def hacer_nodo(estado, i_padre):
     d=distancia_al_objetivo(estado)
-    if i_padre==-1: 
-        n=[estado,0, -1, 1,0,0,d ] 
+    if i_padre==-1:
+        n=[estado,0, -1, 1,0,0,d ]
     else:
         c=nodos[i_padre][4]+costo_entre_dos(estado,nodos[i_padre][0])
         n=[estado,len(nodos), i_padre, nodos[i_padre][3]+1, c, c+d]
@@ -75,11 +81,11 @@ def dentro_de(estado, lista_de_estados):
     rep=0
     for y in lista_de_estados:
         eq=0
-        for i in range(estado):
+        for i in range(len(estado)):
             if estado[i]!=y[i]:
                 break
             eq=eq+1
-        if eq==estado:    
+        if eq==estado:
             return 1
     return 0
 
@@ -102,10 +108,10 @@ solucion=[]
 frontera.append(hacer_nodo(estado_inicial, -1))
 
 while len(frontera)!=0:
-    if E=="1": 
-        nodo= frontera.pop() 
-    else: 
-        nodo= frontera.pop(0) 
+    if E=="1":
+        nodo= frontera.pop()
+    else:
+        nodo= frontera.pop(0)
     visto.append(nodo[0])
    
    
@@ -117,14 +123,15 @@ while len(frontera)!=0:
         solucion=reconstruye(nodo)
         
         en_pantalla(solucion)
-        
+        pintar_camino(solucion)
         break
     
     else:
         sucesores = funcion_sucesor(nodo[0])
         for x in sucesores:
-            if  dentro_de(x,visto)==0: 
+            if  (x in visto)==False:
                 nuevo_nodo= hacer_nodo(x,nodo[1])
+                visto.append(x)
                 if E=="4":
                     cuenta=len(frontera)
                     for i in range(cuenta):
@@ -142,5 +149,5 @@ while len(frontera)!=0:
                     if cuenta== len(frontera):
                         frontera.append(nuevo_nodo)
                 if E=="1" or E=="2":
-                    frontera.append(nuevo_nodo) 
+                    frontera.append(nuevo_nodo)
 print("búsqueda finalizada con {} nodos explorados".format(len(nodos)))
